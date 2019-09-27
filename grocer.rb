fe9bdc8d04a829b115fceea66c1035676526c829
Mez Charney <mez.charney@gmail.com>
2019-09-27 20:51:39 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -15,7 +15,8 @@ end
 def apply_coupons(cart, coupons)
   coupons.each do |coupon|
     item = coupon[:item]
-  if cart[item] && coupon[:item][:coupon] >= coupon[:num] && cart["#{item} W/COUPON"]
+  if cart[item] && coupon[:item][:coupon] >= coupon[:num] && !cart["#{item} W/COUPON"]
+    cart
 end
 end
 end
