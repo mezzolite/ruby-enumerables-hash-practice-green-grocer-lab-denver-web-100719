737e4d29a31403aa16ced7d36f7f653045bc23d9
Mez Charney <mez.charney@gmail.com>
2019-09-27 21:17:45 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -13,12 +13,10 @@ def consolidate_cart(cart)
 end
 
 def apply_coupons(cart, coupons) 
-  
   coupons.each do |coupon| 
     coupon.each do |attribute, value| 
       name = coupon[:item] 
-    
-      if cart[name] && cart[name][:count] >= coupon[:num] 
+    if cart[name] && cart[name][:count] >= coupon[:num] 
         if cart["#{name} W/COUPON"] 
           cart["#{name} W/COUPON"][:count] += 1 
         else 
