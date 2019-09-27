65e3ded6f9fbefe454cef068e8567de617a878b9
Mez Charney <mez.charney@gmail.com>
2019-09-27 21:01:25 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -18,7 +18,7 @@ def apply_coupons(cart, coupons)
   if cart[item] && coupon[:item][:coupon] >= coupon[:num] && !cart["#{item} W/COUPON"]
     cart["#{item} W/COUPON"] = {price: coupon[:cost] / coupon[:num], clearance: cart[item][:clearance], count: coupon[:num] }
     cart[item][:count] -= coupon[:num]
-  elsif
+  elsif coupon[:item][:coupon] >= coupon[:num] && cart["#{item} W/COUPON"]
 end
 end
 end
