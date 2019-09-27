0026a04844c51552046e23dd77885c2bc89f29f0
Mez Charney <mez.charney@gmail.com>
2019-09-27 21:49:00 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -38,5 +38,6 @@ end
 
 def checkout(cart, coupons)
   hash_cart = consolidate_cart(cart)
-  applied_coupons = apply_coupons(hash_cart)
+  applied_coupons = apply_coupons(hash_cart, coupons)
+  applied_clearence = apply_clearance()
 end
